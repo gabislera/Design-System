06c88d0996964f6602335287b80453250d2f2b18
Gabriela Carniel <gabiicarniel@gmail.com>
2023-08-23 15:23:03 -0300

    add: components, fix: docs

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facilis sapiente, ipsa dolorem pariatur expedita dolores eius iste quasi non! Accusantium nulla rem blanditiis eveniet cupiditate beatae id impedit quisquam',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
